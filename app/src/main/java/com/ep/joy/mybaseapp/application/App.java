de971aaea15c6403a2bc91c2646544224a2a363d
依然__饭特稀 <707698592@qq.com>
2016-03-16 17:00:19 +0800

    end

--- app/src/main/java/com/ep/joy/mybaseapp/application/App.java
+++ app/src/main/java/com/ep/joy/mybaseapp/application/App.java
@@ -3,7 +3,9 @@ package com.ep.joy.mybaseapp.application;
 import android.app.Application;
 
 import com.bugtags.library.Bugtags;
+import com.ep.joy.mybaseapp.BuildConfig;
 import com.facebook.stetho.Stetho;
+import com.jiongbull.jlog.JLog;
 
 /**
  * author   Joy
@@ -15,8 +17,15 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        Bugtags.start("daf756fd3bc68a71cb8985d33899601f", this, Bugtags.BTGInvocationEventBubble);
+//        BTGInvocationEventNone    // 静默模式，只收集 Crash 信息（如果允许）
+//
+//        BTGInvocationEventShake   // 通过摇一摇呼出 Bugtags
+//
+//        BTGInvocationEventBubble  // 通过悬浮小球呼出 Bugtags
+        JLog.init(this)
+                .setDebug(BuildConfig.DEBUG);
 
+        Bugtags.start("daf756fd3bc68a71cb8985d33899601f", this, Bugtags.BTGInvocationEventBubble);
         Stetho.initialize(
                 Stetho.newInitializerBuilder(this)
                         .enableDumpapp(
